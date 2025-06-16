079bcb62ec7ec4fb33cafcf6d1eb740653e077ae
Jagadeesha <jaga.m.gowda@gmail.com>
2025-06-16 21:10:50 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='us_visa',
+    version="0.0.0",
+    author="Jagadeesha",
+    author_email="jaga.m.gowda@gmail.com",
+    packages=find_packages()
+)
